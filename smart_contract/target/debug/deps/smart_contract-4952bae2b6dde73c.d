798271cda3f2b130bce8badd5427e7642d4d5cdc
Paul Fresnel <paul.fresnel1@gmail.com>
2023-10-26 10:19:14 +0200

    Improved Smart Contract Folder Structure

---
+++ smart_contract/target/debug/deps/smart_contract-4952bae2b6dde73c.d
@@ -0,0 +1,6 @@
+/Users/paulfresnel/Desktop/web3-projects/coreum/coreum-hackathon/token-titans/smart_contract/target/debug/deps/smart_contract-4952bae2b6dde73c.rmeta: src/lib.rs src/helpers.rs
+
+/Users/paulfresnel/Desktop/web3-projects/coreum/coreum-hackathon/token-titans/smart_contract/target/debug/deps/smart_contract-4952bae2b6dde73c.d: src/lib.rs src/helpers.rs
+
+src/lib.rs:
+src/helpers.rs:
